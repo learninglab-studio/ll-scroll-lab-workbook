7eaeeabfb991c9dd01beacdb9f5c09f54a214441
ll-studio <ll-studio@ll-studios-mbp.lan>
2022-10-15 22:20:52 -0400

    Updates:

--- src/components/video/YouTubeBackground.js
+++ src/components/video/YouTubeBackground.js
@@ -1,15 +1,22 @@
 import styles from './YouTubeBackground.module.css'
 import ReactPlayer from 'react-player'
+import { useEffect, useState } from 'react'
 
 const YouTubeBackground = ({url}) => {
+    const [hasWindow, setHasWindow] = useState()
+    useEffect(() => {
+        if (typeof window !== undefined) {
+            setHasWindow(true)
+        }
+    }, [])
     return (
         <div className={styles.playerWrapper}>
-            <ReactPlayer
+            {hasWindow && <ReactPlayer
                 className={styles.reactPlayer}
                 url={url}
                 width='100%'
                 height='100vh'
-            />
+            />}
         </div>
     )
 }
--- src/components/video/YouTubeForVoting.js
+++ src/components/video/YouTubeForVoting.js
@@ -1,25 +1,34 @@
 import styles from './YouTubeForVoting.module.css'
 import ReactPlayer from 'react-player'
 import Draggable from 'react-draggable'
+import { useEffect, useState } from 'react'
+
 
 const YouTubeForVoting = ({url}) => {
+    const [hasWindow, setHasWindow] = useState()
+    useEffect(() => {
+        if (typeof window !== undefined) {
+            setHasWindow(true)
+        }
+    }, [])
     return (
         <div style={{padding: "20px"}}>
-            <Draggable>
-                <div style={{maxWidth: "150px", alignContent: "right"}}>
-                    <div className={styles.playerWrapper}>
-                        <ReactPlayer
-                            className={styles.reactPlayer}
-                            url={url}
-                            height="110px"
-                            width="150px"
-                            controls={false}
-                        />
+            { hasWindow && 
+                <Draggable>
+                    <div style={{maxWidth: "150px", alignContent: "right"}}>
+                        <div className={styles.playerWrapper}>
+                            <ReactPlayer
+                                className={styles.reactPlayer}
+                                url={url}
+                                height="110px"
+                                width="150px"
+                                controls={false}
+                            />
+                        </div>
+                        <div className={styles.dragHandle}>drag</div>
                     </div>
-                    <div className={styles.dragHandle}>drag</div>
-                </div>
-                
-        </Draggable>
+                </Draggable>
+            }
         </div>
        
     )
